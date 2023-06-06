7b8eb234f9382c1f66c0fbf023229173d80d1df8
Andriikryl <andreyk557@gmail.com>
2023-06-06 12:45:24 +0300

    add maping images into page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      {[...Array(7).keys()].map((_, index) => {
+        return <img key={index} alt="image" src={`/images/${index}.png`}></img>;
+      })}
+    </main>
+  );
 }
